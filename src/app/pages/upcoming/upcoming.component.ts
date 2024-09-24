313bb67ea9c35130ec0a8e5dc1c56d638fb7a51b
eMKey <kartsevnik@gmail.com>
2024-09-24 13:40:48 +0200

    fix component up coming

--- src/app/pages/upcoming/upcoming.component.ts
+++ src/app/pages/upcoming/upcoming.component.ts
@@ -4,7 +4,7 @@ import { DataService } from '../../services/data.service';
 import { DataHandlerService } from '../../services/data-handler.service';
 import { Observable } from 'rxjs';
 import { AppState } from '../../store/state';
-import { selectPopularCurrentPage, selectPopularLoading, selectPopularMovies } from '../../store/selectors';
+import { selectUpComingCurrentPage, selectUpComingLoading, selectUpComingMovies } from '../../store/selectors';
 import { Store } from '@ngrx/store';
 import * as MoviesActions from '../../store/actions';
 
@@ -21,17 +21,17 @@ export class UpcomingComponent {
 
   //Store: Инжектируется в компонент для взаимодействия с хранилищем. Используется для отправки действий и подписки на изменения состояния.
   constructor(private store: Store<AppState>, private dataHandlerService: DataHandlerService) {
-    this.upComingMovies$ = this.store.select(selectPopularMovies); //select: Метод, который выбирает часть состояния из хранилища, используя селекторы.
-    this.currentPage$ = this.store.select(selectPopularCurrentPage);
-    this.isLoading$ = this.store.select(selectPopularLoading);
+    this.upComingMovies$ = this.store.select(selectUpComingMovies); //select: Метод, который выбирает часть состояния из хранилища, используя селекторы.
+    this.currentPage$ = this.store.select(selectUpComingCurrentPage);
+    this.isLoading$ = this.store.select(selectUpComingLoading);
   }
 
   ngOnInit() {
     this.dataHandlerService.changeCategory('Up Coming');
-    this.store.dispatch(MoviesActions.loadPopularMovies()); // dispatch: Метод, который отправляет действие в хранилище для изменения состояния.
+    this.store.dispatch(MoviesActions.loadUpComingMovies()); // dispatch: Метод, который отправляет действие в хранилище для изменения состояния.
   }
 
   loadNextPage() {
-    this.store.dispatch(MoviesActions.loadPopularMovies());  // Загружаем следующую страницу
+    this.store.dispatch(MoviesActions.loadUpComingMovies());  // Загружаем следующую страницу
   }
 }
